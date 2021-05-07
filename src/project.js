784af73bd12522f2eb373e2f6bac83a98839f669
Billo Diallo <billodiallonet@gmail.com>
2021-05-07 22:25:19 +0300

    Add render function

--- src/project.js
+++ src/project.js
@@ -44,6 +44,22 @@ import {
               listsContainer.appendChild(listElement);
             });
           }
+
+          function render() {
+            clearElement(listsContainer);
+            renderLists();
+        
+            const selectedList = lists.find((list) => list.id === selectedListId);
+            if (selectedListId == null) {
+              listDisplayContainer.style.display = 'none';
+            } else {
+              listDisplayContainer.style.display = '';
+              listTitleElement.innerText = selectedList.name;
+              renderTaskCount(selectedList);
+              clearElement(tasksContainer);
+              renderTasks(selectedList);
+            }
+          }
         
       
 
