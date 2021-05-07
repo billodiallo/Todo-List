9f2bc2065d79fd9ac4793439395ba847a05b3481
Billo Diallo <billodiallonet@gmail.com>
2021-05-07 22:07:16 +0300

    ADD create project

---
+++ src/project.js
@@ -0,0 +1,17 @@
+import {
+    renderTaskCount, createList, createTask, clearElement,
+  } from './logic';
+
+  const createProject = () => {
+    const listsContainer = document.querySelector('[data-lists]');
+    const newListForm = document.querySelector('[data-new-list-form]');
+    const newListinput = document.querySelector('[data-new-list-input]');
+    const deleteListButton = document.querySelector('[data-delete-list-button]');
+    const listDisplayContainer = document.querySelector('[data-list-display-container]');
+    const listTitleElement = document.querySelector('[data-list-title]');
+    const tasksContainer = document.querySelector('[data-tasks]');
+    const taskTemplate = document.getElementById('task-template');
+    const newTaskForm = document.querySelector('[data-new-task-form]');
+    const newTaskInput = document.querySelector('[data-new-task-input]');
+    const clearCompleteTasksButton = document.querySelector('[data-clear-complete-tasks-button]');
+  }
