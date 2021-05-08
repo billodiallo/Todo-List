a4ed61badd0deac4ac4ffca40d438458f17dcd12
Billo Diallo <billodiallonet@gmail.com>
2021-05-08 12:31:46 +0300

    Add positive and negative for for createTask

--- src/logic.spec.js
+++ src/logic.spec.js
@@ -1,5 +1,9 @@
 import { createList, createTask, clearElement } from './logic';
 
 test('Check name', () => {
-    expect(createTask('billo').name).toBe('billo');
-  });
+  expect(createTask('billo').name).toBe('billo');
+});
+
+test('Check negative scenario', () => {
+  expect(createTask('bb').complete).toBe(false);
+});
