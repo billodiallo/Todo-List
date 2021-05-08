ca33f4adecda647bc878ce42c7a42502b7aa35c0
Billo Diallo <billodiallonet@gmail.com>
2021-05-08 05:39:33 +0300

    Fix linters errors

--- src/logic.js
+++ src/logic.js
@@ -4,23 +4,21 @@ function createList(name) {
   return { id: Date.now().toString(), name, tasks: [] };
 }
 function createTask(name) {
-    return { id: Date.now().toString(), name, complete: false };
-  }
+  return { id: Date.now().toString(), name, complete: false };
+}
 
-  function clearElement(element) {
-    while (element.firstChild) {
-      element.removeChild(element.firstChild);
-    }
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
   }
+}
 
-  function renderTaskCount(selectedListId) {
-    const incompleteTaskCount = selectedListId.tasks.filter((task) => !task.complete).length;
-    const taskString = incompleteTaskCount === 1 ? 'task' : 'tasks';
-    listCountElement.innerText = `${incompleteTaskCount} ${taskString} remaining`;
-  }
+function renderTaskCount(selectedListId) {
+  const incompleteTaskCount = selectedListId.tasks.filter((task) => !task.complete).length;
+  const taskString = incompleteTaskCount === 1 ? 'task' : 'tasks';
+  listCountElement.innerText = `${incompleteTaskCount} ${taskString} remaining`;
+}
 
-  export {
-    createList, createTask, clearElement, renderTaskCount,
-  };
-  
-  
+export {
+  createList, createTask, clearElement, renderTaskCount,
+};
