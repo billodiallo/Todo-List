0d0864b164c9ead7e844a8a966865fb2295fcde0
Billo Diallo <billodiallonet@gmail.com>
2021-05-18 12:34:42 +0300

    Test add new project feature

--- test/todo.spec.js
+++ test/todo.spec.js
@@ -1,2 +1,12 @@
 import Todo from '../src/todo';
 import Project from '../src/project';
+
+const myProject = new Project('myTitle');
+describe('change attr', () => {
+    const myTodo = new Todo('myTodoTitle', 'myDescription', '2021-04-21', 1, 0);
+  
+    test('it change the title when given "title" as an argument and a new title', () => {
+      myTodo.changeAttr('title', 'myTodoNewTitle');
+      expect(myTodo.title).toBe('myTodoNewTitle');
+    });
+});  
